bc74b5ad95da76bfa40939025aad01ea57ec50c5
Olha Bahatiuk <olha@nanopay.net>
2021-05-17 12:26:31 -0400

    js_build file coppied

---
+++ js_build.js
@@ -0,0 +1,34 @@
+var npRoot = __dirname + '/../';
+
+require(npRoot + 'foam3/src/foam.js');
+require(__dirname + '/Config.js');
+
+var config  = foobar.Config.create();
+
+function hello_world(userName) {
+  // console.log('hello ' + userName);
+
+  config.build = true;
+
+  help();  
+
+  // console.log(config);
+}
+
+function help() {
+  console.log(foobar.Config.getAxioms().filter(p => p.help).map(p => p.help));
+  // foam.__context__.classloader.load('foobar.Config').then(function(cls) {
+  //   console.log(cls.create().cls_.getAxioms().map(p => p.help));
+  // });
+}
+
+
+//create config obj here
+var opts = {};
+
+opts.alias = { name: 'n' };
+
+var argv = require('minimist')(process.argv.slice(2), opts);
+
+help();
+config.clean();
